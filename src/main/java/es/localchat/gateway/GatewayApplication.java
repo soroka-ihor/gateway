78c2bffd1e69c4960a6fc2fbf7ca5dff4db72248
Ihor Soroka <ihor.soroka@inmost.pro>
2024-12-18 12:19:07 +0200

    fixes

--- src/main/java/es/localchat/gateway/GatewayApplication.java
+++ src/main/java/es/localchat/gateway/GatewayApplication.java
@@ -17,21 +17,22 @@ public class GatewayApplication {
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        var methods = new String[] { "GET", "POST", "PUT", "DELETE", "OPTIONS" };
         // adding 2 rotes to first microservice as we need to log request body if method is POST
         return builder.routes()
-                .route("first-microservice",r -> r.path("/first")
-                        .and().method("POST")
-                        .and().readBody(Student.class, s -> true).filters(f -> f.filters(requestFilter, authFilter))
+                .route("general-service",r -> r.path("/api/v1/user")
+                        .and().method(methods)
+                        //.and().readBody(Student.class, s -> true).filters(f -> f.filters(requestFilter, authFilter))
                         .uri("http://localhost:8081"))
                 .route("first-microservice",r -> r.path("/first")
-                        .and().method("GET").filters(f-> f.filters(authFilter))
+                        //.and().method("GET").filters(f-> f.filters(authFilter))
                         .uri("http://localhost:8081"))
                 .route("second-microservice",r -> r.path("/second")
                         .and().method("POST")
-                        .and().readBody(Company.class, s -> true).filters(f -> f.filters(requestFilter, authFilter))
+                        //.and().readBody(Company.class, s -> true).filters(f -> f.filters(requestFilter, authFilter))
                         .uri("http://localhost:8082"))
                 .route("second-microservice",r -> r.path("/second")
-                        .and().method("GET").filters(f-> f.filters(authFilter))
+                        //.and().method("GET").filters(f-> f.filters(authFilter))
                         .uri("http://localhost:8082"))
                 .route("auth-server",r -> r.path("/login")
                         .uri("http://localhost:8088"))
